d7c373b8087c218279ddaea658acee65e18c442b
Gabriel Pires <gabpiresdev@gmail.com>
2023-08-23 21:47:01 -0300

    feat: dark theme to sidebar component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,10 @@ const button = tv({
     variant: {
       primary: ['bg-violet-600 text-white', 'hover:bg-violet-700'],
       outline: ['border border-zinc-300 text-zinc-700', 'hover:bg-zinc-50'],
-      ghost: ['rounded-md px-2', 'hover:bg-zinc-50 text-zinc-500 shadow-none'],
+      ghost: [
+        'rounded-md px-2',
+        'hover:bg-zinc-50 text-zinc-500 shadow-none dark:hover:bg-zinc-800 dark:text-zinc-400',
+      ],
     },
   },
   defaultVariants: {
